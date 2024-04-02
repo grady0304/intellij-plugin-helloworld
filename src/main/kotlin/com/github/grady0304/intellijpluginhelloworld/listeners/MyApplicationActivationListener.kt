b92ed5d7b9639fd07bcc56344890b67ee37fc1b7
GitHub Action <action@github.com>
2024-04-02 06:46:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/grady0304/intellijpluginhelloworld/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/grady0304/intellijpluginhelloworld/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.grady0304.intellijpluginhelloworld.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/grady0304/intellijpluginhelloworld/services/MyProjectService.kt
+++ src/main/kotlin/com/github/grady0304/intellijpluginhelloworld/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.grady0304.intellijpluginhelloworld.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.grady0304.intellijpluginhelloworld.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
